97901cf6c0f7d4c644268d63117953fba45668a2
Hammond, Robert <Robert.Hammond@nrel.gov>
2021-06-18 15:39:14 -0600

    add package data flag

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     ],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
-    package_data={"": ["library/*"]},
+    package_data={"wombat": ["library/*"]},
     install_requires=[
         "attr",
         "numpy",
