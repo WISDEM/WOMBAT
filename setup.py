81241f052950e9dcb791398bac5d755d1c9ee78f
Hammond, Robert <Robert.Hammond@nrel.gov>
2021-06-18 16:27:56 -0600

    update package data

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     ],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
-    package_data={"": ["library/*"]},
+    package_data={"": ["library/*.yaml", "library/*.csv"]},
     install_requires=[
         "attr",
         "numpy",
