c57eb8c80669c04b66b52011649355fbc81754ad
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2021-12-08 12:17:29 -0700

    fix bad isort fix

--- wombat/windfarm/system/__init__.py
+++ wombat/windfarm/system/__init__.py
@@ -1,6 +1,6 @@
 """Initializes the Turbine and Subassembly classes."""
 
 
-from .cable import Cable
-from .system import System
-from .subassembly import Subassembly
+from .cable import Cable  # isort: skip
+from .subassembly import Subassembly  # isort: skip
+from .system import System  # isort: skip
