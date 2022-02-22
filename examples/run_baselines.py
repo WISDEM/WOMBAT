49c1a76c71ad4486d86906504a781fa00b86379e
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-02-22 14:22:17 -0700

    update script and outputs

--- examples/run_baselines.py
+++ examples/run_baselines.py
@@ -2,6 +2,9 @@
 
 NOTES:
     - You will need xlwings to run this script, which can be installed with: `pip install xlwings`
+    - Ensure your git repository is up to date with:
+        1) `git fetch origin`; replace origin with other remote/upstream as needed
+        2) `git pull origin`; replace origin with other remote/upstream as needed
 
 HOW TO:
     1) Update the library data for the baseline inputs
@@ -9,8 +12,10 @@ HOW TO:
 """
 
 
+from pprint import pprint
 from pathlib import Path
 
+import numpy as np
 import xlwings as xw
 
 from wombat.core import Simulation
@@ -21,6 +26,114 @@ here = Path(__file__).resolve().parent
 wb = xw.Book(here / "WOMBAT_baselines.xlsx")
 
 
+def compute_results_table(sim):
+    """Creates the results table that gets printed to Excel for each simulation."""
+    metrics = sim.metrics
+
+    avail_time = [metrics.time_based_availability(frequency="project", by="windfarm")]
+    avail_time.extend(
+        metrics.time_based_availability(
+            frequency="annual", by="windfarm"
+        ).values.flatten()
+    )
+    avail_time = np.array(avail_time) * 100.0
+
+    avail_prod = [
+        metrics.production_based_availability(frequency="project", by="windfarm")
+    ]
+    avail_prod.extend(
+        metrics.production_based_availability(
+            frequency="annual", by="windfarm"
+        ).values.flatten()
+    )
+    avail_prod = np.array(avail_prod) * 100.0
+
+    power_production = [
+        metrics.power_production(frequency="project", by_turbine=False).values[0][0]
+    ]
+    power_production.extend(
+        metrics.power_production(frequency="annual", by_turbine=False).values.flatten()
+    )
+    power_production = np.array(power_production) / 1000.0
+
+    cf_net = [metrics.capacity_factor(which="net", frequency="project", by="windfarm")]
+    cf_net.extend(
+        metrics.capacity_factor(
+            which="net", frequency="annual", by="windfarm"
+        ).values.flatten()
+    )
+    cf_net = np.array(cf_net) * 100.0
+
+    cf_gross = [
+        metrics.capacity_factor(which="gross", frequency="project", by="windfarm")
+    ]
+    cf_gross.extend(
+        metrics.capacity_factor(
+            which="gross", frequency="annual", by="windfarm"
+        ).values.flatten()
+    )
+    cf_gross = np.array(cf_gross) * 100.0
+
+    completion_rate = [metrics.task_completion_rate(which="both", frequency="project")]
+    completion_rate.extend(
+        metrics.task_completion_rate(which="both", frequency="annual").values.flatten()
+    )
+    completion_rate = np.array(completion_rate) * 100.0
+
+    component_costs = metrics.component_costs(frequency="annual", by_category=True)
+    component_costs = component_costs.droplevel("component").groupby("year").sum()
+    materials = [component_costs.materials_cost.sum()]
+    materials.extend(component_costs.materials_cost.values.tolist())
+    materials = np.array(materials) / 1e6
+
+    equipment = [metrics.equipment_costs(frequency="project", by_equipment=False)]
+    equipment.extend(
+        metrics.equipment_costs(frequency="annual", by_equipment=False).values.flatten()
+    )
+    equipment = np.array(equipment) / 1e6
+
+    df = metrics.equipment_costs(frequency="annual", by_equipment=True)
+    equipment_breakdown = [
+        [f"{col} Cost", "Milllions USD$", df[col].sum() / 1e6, *(df[col].values / 1e6)]
+        for col in df.columns
+    ]
+
+    equipment_utilization = metrics.service_equipment_utilization(frequency="project")
+    df = metrics.service_equipment_utilization(frequency="annual")
+    equipment_utilization = [
+        [
+            f"{col} Utilization",
+            "%",
+            equipment_utilization[col].values[0] * 100.0,
+            *(df[col].values.flatten() * 100.0),
+        ]
+        for col in equipment_utilization.columns
+    ]
+
+    results_table = [
+        [
+            "Metric",
+            "Units",
+            "Project Total",
+            *[f"Year {i}" for i in range(1, len(avail_time))],
+        ],
+        ["Time-based Availability", "%", *avail_time],
+        ["Production-based Availability", "%", *avail_prod],
+        ["Power Production", "MW", *power_production],
+        ["Net Capacity Factor", "%", *cf_net],
+        ["Gross Capacity Factor", "%", *cf_gross],
+        ["Task Completion Rate", "%", *completion_rate],
+        ["Materials Cost", "Millions USD$", *materials],
+        ["Total Servicing Equipment Cost", "Millions USD$", *equipment],
+        *equipment_breakdown,
+        *equipment_utilization,
+    ]
+    pprint(results_table)
+    for row in results_table:
+        print(row[0], len(row))
+    return results_table
+
+
 # LBW Base Case
 
 
@@ -29,7 +142,7 @@ def run_lbw():
     data points.
     """
     print("LBW starting")
-    sim_lbw = Simulation(library_path="LBW", config="base.yaml")
+    sim_lbw = Simulation(library_path="LBW", config="base.yaml")  # type: ignore
     print("LBW data loaded")
 
     sim_lbw.run()
@@ -82,6 +195,10 @@ def run_lbw():
     lbw_sheet.range("A18").expand().value = data_table
 
     # Write the output data to file
+    lbw_sheet = wb.sheets["LBW Results"]
+    results_table = compute_results_table(sim_lbw)
+    lbw_sheet.range("A1").expand().value = ""
+    lbw_sheet.range("A1").value = results_table
 
     # Cleanup
     sim_lbw.env.cleanup_log_files()
@@ -92,10 +209,10 @@ def run_osw_fixed():
     and output data points.
     """
     print("OSW-Fixed starting")
-    sim_osw_fixed = Simulation(library_path="OSW_FIXED", config="base.yaml")
+    sim_osw_fixed = Simulation(library_path="OSW_FIXED", config="base.yaml")  # type: ignore
     print("OSW-Fixed data loaded")
 
-    # sim_osw_fixed.run()
+    sim_osw_fixed.run()
     print("OSW-Fixed simulation complete")
 
     # Write the input data to file
@@ -144,7 +261,11 @@ def run_osw_fixed():
     osw_sheet = wb.sheets["OSW"]
     osw_sheet.range("A19").expand().value = data_table
 
-    # Write the output data
+    # Write the output data to file
+    osw_sheet = wb.sheets["OSW Results"]
+    results_table = compute_results_table(sim_osw_fixed)
+    osw_sheet.range("A1").expand().value = ""
+    osw_sheet.range("A1").value = results_table
 
     # Cleanup
     sim_osw_fixed.env.cleanup_log_files()
