7793273b4d1dc4f733251d0783d3adad89707a9a
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-06-23 15:06:31 -0600

    small update to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,9 +91,9 @@ source_suffix = {
 jupyter_execute_notebooks = "auto"
 execution_timeout = -1
 execution_allow_errors = True
-execution_excludepatterns = [
-    "*_demonstration.ipynb"
-]  # only used for quick docs building when updating the how to example
+# execution_excludepatterns = [
+#     "*_demonstration.ipynb"
+# ]  # only used for quick docs building when updating the how to example
 
 myst_heading_anchors = 3
 
