bb3f3de6dd94710276addf28be422d04c18bcc37
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2023-12-18 10:33:04 -0800

    PATCH: Fix Minor TOML Formatting Bug (#133)
    * add missing square brackets
    
    * add preferred version management
    
    * update pre-commit versioning
    
    * remove outdated docs configuration
    
    * remove unused import

--- wombat/__init__.py
+++ wombat/__init__.py
@@ -1,9 +1,7 @@
 """Package initialization."""
 
-import importlib.metadata
-
 from wombat.core import Metrics, Simulation
 from wombat.core.library import create_library_structure
 
 
-__version__ = importlib.metadata.version("wombat")
+__version__ = "0.9.2"
