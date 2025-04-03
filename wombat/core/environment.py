410357c8c3214c703caf71dc6e8bf822e656e91e
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2025-04-03 14:49:23 -0700

    fix the soon-to-be-deprecated offset code

--- wombat/core/environment.py
+++ wombat/core/environment.py
@@ -504,7 +504,7 @@ class WombatEnvironment(simpy.Environment):
                 .fillna(0.0)
                 .set_index("datetime")
                 .sort_index()
-                .resample("H")
+                .resample("h")
                 .interpolate(limit_direction="both")  # , limit=5)
                 .reset_index(drop=False)
             )
