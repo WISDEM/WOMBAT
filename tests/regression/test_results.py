01364e7ef4c873ec4670bb7ecc3d478be3bb7d93
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2025-05-28 16:58:35 -0700

    split tests between unit and regression

--- tests/regression/test_results.py
+++ tests/regression/test_results.py
@@ -1,32 +1,7 @@
 """Test the post-processing functionality."""
 
-import pytest
 import pandas.testing as pdt
 from pytest_check import check
-
-from wombat.core import Frequency
-from wombat.core.post_processor import _check_frequency
-
-
-def test_check_frequency():
-    """Test `_check_frequency`."""
-    assert _check_frequency("project").value == "project"
-    assert _check_frequency("annual").value == "annual"
-    assert _check_frequency("monthly").value == "monthly"
-    assert _check_frequency("month year").value == "month_year"
-
-    frequency = _check_frequency("project")
-    assert isinstance(frequency, Frequency)
-    assert frequency is Frequency.PROJECT
-
-    with pytest.raises(ValueError):
-        _check_frequency("annual", which="project")
-
-    with pytest.raises(ValueError):
-        _check_frequency("monthly", which="project")
-
-    with pytest.raises(ValueError):
-        _check_frequency("month-year", which="project")
 
 
 def test_results_consistency(setup_ttp):
