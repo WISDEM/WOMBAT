ff9b9b1279e256d86a9629c20dafa8905c704bf9
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2025-05-28 16:49:35 -0700

    enable split between unit and regression testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,41 @@ from wombat.utilities import IEC_power_curve
 from wombat.core.library import load_yaml
 
 
-TEST_DATA = Path(__file__).resolve().parent / "library"
+ROOT = Path(__file__).parent
+TEST_DATA = ROOT / "library"
+
+
+def pytest_addoption(parser):  # noqa: D103
+    parser.addoption(
+        "--unit", action="store_true", default=False, help="run tests in test/unit/."
+    )
+    parser.addoption(
+        "--regression",
+        action="store_true",
+        default=False,
+        help="run tests in test/regression/.",
+    )
+
+
+def pytest_configure(config):  # noqa: D103
+    # Check for the options
+    unit = config.getoption("--unit")
+    regression = config.getoption("--regression")
+
+    # Provide the appropriate directories
+    unit_tests = [el for el in (ROOT / "unit").iterdir() if el.suffix == ".py"]
+    regression_tests = [
+        el for el in (ROOT / "regression").iterdir() if el.suffix == ".py"
+    ]
+
+    # If both, run them all; if neither skip any modifications; otherwise run just the
+    # appropriate subset
+    if regression and unit:
+        config.args = unit_tests + regression_tests
+    elif regression:
+        config.args = regression_tests
+    elif unit:
+        config.args = unit_tests
 
 
 def pytest_assertrepr_compare(op, left, right):
