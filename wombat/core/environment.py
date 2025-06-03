8d79730eaaa0056687e3a3067a50135abfc2c079
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2025-06-03 14:18:19 -0700

    update electrolyzer potential to be based on windfarm production, not potential

--- wombat/core/environment.py
+++ wombat/core/environment.py
@@ -930,15 +930,6 @@ class WombatEnvironment(simpy.Environment):
             env_time=operations.env_time.values,
             env_datetime=operations.env_datetime.values,
         )
-        if electrolyzers := windfarm.electrolyzer_id:
-            potential_df[electrolyzers] = np.vstack(
-                [
-                    windfarm.system(e_id).power(potential_df.windfarm)
-                    for e_id in electrolyzers
-                ]
-            ).T
-
-        potential_df.to_parquet(self.power_potential_fname)
 
         # TODO: The actual windfarm production needs to be clipped at each subgraph to
         # the max of the substation's operating capacity and then summed.
@@ -947,9 +938,25 @@ class WombatEnvironment(simpy.Environment):
         production_df.windfarm = self._calculate_adjusted_production(
             operations, production_df
         )
-        if electrolyzers:
-            potential_df[electrolyzers] *= operations[electrolyzers].values
 
+        if electrolyzers := windfarm.electrolyzer_id:
+            potential_df[electrolyzers] = np.vstack(
+                [
+                    windfarm.system(e_id).power(production_df.windfarm)
+                    for e_id in electrolyzers
+                ]
+            ).T
+            production_df[electrolyzers] = (
+                np.vstack(
+                    [
+                        windfarm.system(e_id).power(production_df.windfarm)
+                        for e_id in electrolyzers
+                    ]
+                ).T
+                * operations[electrolyzers].values
+            )
+
+        potential_df.to_parquet(self.power_potential_fname)
         production_df.to_parquet(self.power_production_fname)
 
         if return_df:
