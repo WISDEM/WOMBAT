ff81d034ee0ba64c03836e52bf6790a766372a4c
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2025-06-18 12:40:45 -0700

    add load_yaml to top level import

--- wombat/__init__.py
+++ wombat/__init__.py
@@ -1,7 +1,7 @@
 """Package initialization."""
 
 from wombat.core import Metrics, Simulation
-from wombat.core.library import create_library_structure
+from wombat.core.library import create_library_structure, load_yaml
 
 
 __version__ = "0.10.4"
