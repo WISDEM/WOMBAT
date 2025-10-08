update fixed costs calculations based on units input

wombat/core/post_processor.py
@@ -1908,11 +1908,9 @@ class Metrics:
 
         # Get the appropriate values and convert to the currency base
         keys = self.fixed_costs.resolution[resolution]
-        vals = (
-            np.array([[getattr(self.fixed_costs, key) for key in keys]])
-            * self.project_capacity
-            * 1000
-        )
+        vals = np.array([[getattr(self.fixed_costs, key) for key in keys]])
+        if self.fixed_costs.units == "$/kw/yr":
+            vals *= self.project_capacity * 1000
 
         total = (
             self.operations[["year", "month", "env_time"]]