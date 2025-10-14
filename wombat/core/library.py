dd1daed0ff15c4636bc81f103f287a11d5d4d792
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2025-10-14 14:50:11 -0700

    update library mapping

--- wombat/core/library.py
+++ wombat/core/library.py
@@ -31,23 +31,18 @@ import yaml
 ROOT = Path(__file__).parents[2].resolve()
 DEFAULT_LIBRARY = ROOT / "library"
 CODE_COMPARISON = DEFAULT_LIBRARY / "code_comparison"
-BASE_CASES = DEFAULT_LIBRARY / "baseline"
+DEFAULT_DATA = DEFAULT_LIBRARY / "default"
 
 DINWOODIE = CODE_COMPARISON / "dinwoodie"
 IEA_26 = CODE_COMPARISON / "iea26"
 COREWIND = DEFAULT_LIBRARY / "corewind"
 
-OSW_FIXED = BASE_CASES / "offshore_fixed"
-LBW = BASE_CASES / "land_based"
-
 library_map = {
+    "DEFAULT": DEFAULT_DATA,
+    "COREWIND": COREWIND,
     "DINWOODIE": DINWOODIE,
     "IEA_26": IEA_26,
     "IEA26": IEA_26,
-    "COREWIND": COREWIND,
-    "OSW_FIXED": OSW_FIXED,
-    "LBW": LBW,
-    "LAND_BASED": LBW,
 }
 
 # YAML loader that is able to read scientific notation
