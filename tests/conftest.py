fcd097901d6a7e0012927ec0a9e5b79763119afe
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2025-12-01 11:12:27 -0800

    update for args list[str] enforcement in v9

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,9 +34,9 @@ def pytest_configure(config):  # noqa: D103
     regression = config.getoption("--regression")
 
     # Provide the appropriate directories
-    unit_tests = [el for el in (ROOT / "unit").iterdir() if el.suffix == ".py"]
+    unit_tests = [str(el) for el in (ROOT / "unit").iterdir() if el.suffix == ".py"]
     regression_tests = [
-        el for el in (ROOT / "regression").iterdir() if el.suffix == ".py"
+        str(el) for el in (ROOT / "regression").iterdir() if el.suffix == ".py"
     ]
 
     # If both, run them all; if neither skip any modifications; otherwise run just the
